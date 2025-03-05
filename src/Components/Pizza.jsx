da4934558a4b79d4de83df2b34b1901e1d612e58
deexitanaik <deexitanaik@gmail.com>
2025-03-05 13:55:37 +0530

    4 commit

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -87,6 +87,63 @@ export const Pizza = () => {
                  <div className="right"><IoIosArrowDroprightCircle /></div>
             </div>
 
+            <div className="varities">
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/6039/6039507.png"></img>
+                    <div className="names">Fruits</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/7649/7649195.png"></img>
+                    <div className="names">Bread</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/6638/6638114.png"></img>
+                    <div className="names1">Vegetable</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/12541/12541268.png"></img>
+                    <div className="names">Fish</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/9616/9616953.png"></img>
+                    <div className="names">Meat</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/16428/16428592.png"></img>
+                    <div className="names">Drinks</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/10775/10775446.png"></img>
+                    <div className="names2">Sea Food</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/6639/6639747.png"></img>
+                    <div className="names3">Ice Creams</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/12608/12608914.png"></img>
+                    <div className="names">Juice</div>
+                </div>
+
+                <div className="item">
+                    <img src="https://cdn-icons-png.flaticon.com/256/10777/10777131.png"></img>
+                    <div className="names">Jam</div>
+                </div>
+            </div>
+
+            <div className="titles">
+                <div className="products">Popular Products</div>
+                <div className="side-name">View More</div>
+            </div>
+
 
             </div>
 
